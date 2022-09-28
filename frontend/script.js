a3ea860b09a3609f439828b0d2ae76ba134f2ce3
Joe <pelikata@gmail.com>
2022-09-28 11:44:24 +0200

    practice

---
+++ frontend/script.js
@@ -0,0 +1,36 @@
+const monthComponent = function(nth, name, days){
+    let daysHtml = "";
+    
+    for (let i = 1; i <= days; i++){
+        daysHtml += dayComponent(i);
+    }
+    return `
+        <section id= "${nth}" class="${name}">
+            <h2>${name}</h2>
+            ${daysHtml}   
+        </section>
+    `
+}
+
+const dayComponent = function(dayCount){
+    return `
+        <div class= "day">${dayCount}</div>
+    `
+}
+
+console.log("hello");
+
+const rootElement = document.querySelector ("#root");
+
+rootElement.insertAdjacentHTML("beforeend", "<button>Show calendar</button>");
+const buttonElement = rootElement.querySelector("button");
+buttonElement.addEventListener("click", function()
+{})
+    rootElement.insertAdjacentHTML("beforeend", monthComponent(1., "January", 31));
+    rootElement.insertAdjacentHTML("beforeend", monthComponent(2., "February", 28));
+    rootElement.insertAdjacentHTML("beforeend", monthComponent(20., "Pityuhónap", 50));
+
+
+
+
+/* rootElement.style.color = "blue"; */
